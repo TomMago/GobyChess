291a67cc84e5055f8dfdf84d9f7be16cc4586e63
TomMago <tom.magorsch@tu-dortmund.de>
2021-03-14 01:57:10 +0100

    sliding collision in all directions

--- gobychess/utils.py
+++ gobychess/utils.py
@@ -2,8 +2,9 @@
 
 from textwrap import wrap
 
-from gmpy2 import xmpz
+from gmpy2 import xmpz, bit_scan1,  bit_clear
 
+EMPTY = xmpz(0b0000000000000000000000000000000000000000000000000000000000000000)
 
 def print_bitboard(board):
     '''
@@ -54,7 +55,7 @@ def generate_direction(direction):
     for i in range(64):
         field_count = i
         attack_board = xmpz(0b0000000000000000000000000000000000000000000000000000000000000000)
-        attack_board[i] = 1
+        attack_board[i] = 0
 
         if(direction == +8):
             condition = lambda x: x < 56
@@ -85,7 +86,75 @@ def generate_direction(direction):
     return directions
 
 
-
 table = generate_table()
 
-print_bitboard(table['south east'][13])
+
+blockers = xmpz(0b0000000000000000000000000000000000000000000000000000000000000000)
+blockers[21] = 1
+blockers[23] = 1
+
+
+def blocker_computation(square, blockers_opposite, blockers_own, direction):
+    collision_opposite = table[direction][square] & blockers_opposite
+    collision_own = table[direction][square] & blockers_own
+    if(direction in ['east', 'north east', 'north', 'north west']):
+        idx_opposite = bit_scan1(collision_opposite) if bit_scan1(collision_opposite) else 66
+        idx_own = bit_scan1(collision_own) if bit_scan1(collision_own) else 66
+    if(direction in ['west', 'south west', 'south', 'south east']):
+        idx_opposite = reverse_bit_scan1(collision_opposite) if reverse_bit_scan1(collision_opposite) else 66
+        idx_own = reverse_bit_scan1(collision_own) if reverse_bit_scan1(collision_own) else 66
+
+    # print("=====================")
+    # print_bitboard(collision_opposite)
+    # print("=====================")
+    # print_bitboard(collision_own)
+    # print("=====================")
+    # print(idx_opposite)
+    # print(idx_own)
+
+    if idx_opposite == 66 and idx_own == 66:
+        blocked = xmpz(0b0000000000000000000000000000000000000000000000000000000000000000)
+    elif idx_opposite < idx_own:
+        idx = idx_opposite
+        blocked = table[direction][idx]
+        #print("opp: " + str(idx))
+    else:
+        idx = idx_own - 1
+        blocked = table[direction][idx]
+        #print("own" + str(idx))
+
+    blocked = (1 << 64) - 1 - blocked
+    movement = table[direction][square] & blocked
+    return movement
+
+
+def rook_sliding(square, blockers_opposite, blockers_own):
+
+    pass
+
+
+def reverse_bit_scan1(bitboard):
+    length = bitboard.bit_length()
+    if length == 0:
+        return None
+    else:
+        return length - 1
+
+
+empty = xmpz(0b0000000000000000000000000000000000000000000000000000000000000000)
+
+print_bitboard(blockers)
+print()
+print_bitboard(blocker_computation(17, blockers, empty, 'east'))
+print()
+print_bitboard(blocker_computation(17, blockers, empty, 'north'))
+print()
+print_bitboard(blocker_computation(17, blockers, empty, 'west'))
+print()
+print_bitboard(blocker_computation(17, blockers, empty, 'south'))
+
+
+
+#rook_sliding(17, empty, blockers)
+#print()
+#rook_sliding(17, blockers, empty)
